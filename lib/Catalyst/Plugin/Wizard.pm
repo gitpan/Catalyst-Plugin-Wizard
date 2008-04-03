30c6594cbf44bfdda5caa192fb492ec379141d17
Pavel Boldin <davinchi@cpan.org>
2008-04-03 20:25:37 +0000

    Import of DAVINCHI/Catalyst-Plugin-Wizard-0.03 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-Wizard
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         DAVINCHI/Catalyst-Plugin-Wizard-0.03.tar.gz
    gitpan-cpan-author:       DAVINCHI
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Plugin/Wizard.pm
+++ lib/Catalyst/Plugin/Wizard.pm
@@ -32,7 +32,7 @@ __PACKAGE__->mk_classdata( 'wizards' => {} );
 
 our $DEBUG = $ENV{CATALYST_WIZARD_DEBUG} || 0;
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 sub instance { 
     shift->config->{wizard}->{instance} 
